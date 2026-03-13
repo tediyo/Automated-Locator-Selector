72b8e7b050ff878089f289ee462d2da3124ef6e4
Tewodros <thedronberhanu05@gmail.com>
2026-03-13 13:13:55 +0300

    configration problem fixed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,14 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*', // For production, you should specify your Vercel URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-  const port = process.env.PORT ?? 3001;
-  await app.listen(port);
-  console.log(`Backend is running on port: ${port}`);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin: '*', // For production, you should specify your Vercel URL
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
+    const port = process.env.PORT ?? 3001;
+    await app.listen(port);
+    console.log(`Backend is running on port: ${port}`);
+  } catch (error) {
+    console.error('Failed to bootstrap the application:', error);
+    process.exit(1);
+  }
 }
 bootstrap();
